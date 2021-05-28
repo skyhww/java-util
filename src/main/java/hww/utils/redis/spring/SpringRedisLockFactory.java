b17e807e5f092bcefc5770452536948c9e2bfe07
hanweiwei <hanweiwei@bixin.cn>
2021-05-28 16:31:10 +0800

    redis lock

--- src/main/java/hww/utils/redis/spring/SpringRedisLockFactory.java
+++ src/main/java/hww/utils/redis/spring/SpringRedisLockFactory.java
@@ -27,7 +27,7 @@ public class SpringRedisLockFactory implements BeanDefinitionRegistryPostProcess
         if (!beanDefinitionRegistry.containsBeanDefinition(SpringRedisLock.class.getName())) {
             BeanDefinitionBuilder beanDefinitionBuilder = BeanDefinitionBuilder.rootBeanDefinition(SpringRedisLock.class);
             beanDefinitionBuilder.setScope("prototype");
-            beanDefinitionRegistry.registerBeanDefinition(beanDefinitionBuilder);
+            beanDefinitionRegistry.registerBeanDefinition(SpringRedisLock.class.getName(), beanDefinitionBuilder.getBeanDefinition());
         }
 
     }
